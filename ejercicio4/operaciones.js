937512a0eea55292b15812b13d8b42f16f6e1301
Yohan Puerta <yohanjjfray@gmail.com>
2020-02-20 21:09:55 -0500

    Se agrega el index.html y operaciones.js del algoritmo número 4 del taller de Nuevas Tecnologías

---
+++ ejercicio4/operaciones.js
@@ -0,0 +1,28 @@
+/*4. Se requiere un algoritmo para determinar, de N cantidades, cuántas son menores o iguales a cero y cuántas mayores a cero.*/
+
+let cantidad = 0;
+let iteraciones = 0;
+let contar = 0;
+
+iteraciones = parseInt (prompt("Cuántas cantidades desea ingresar ?"));
+
+for(i=0;i<iteraciones;i++){
+   estatura = parseInt (prompt("Ingresar estatura de la persona: "+contarPersonas));
+   if(cantidad>0){
+
+   }eñse{
+
+   }
+}
+
+do{
+    
+   //Se valida que el valor que se captura sea un número mayor a cero y que no sea una letra.
+   //Si no cumple alguno de estos requisitos no se acumula ni se cuenta el usuario
+   if(estatura>0){
+      sumaEstatura += estatura;
+      contarPersonas ++;
+   }
+ }while(estatura>0);
+
+ document.writeln("El promedio de estatura es: "+ (sumaEstatura/contarPersonas));
